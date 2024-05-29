28707b6394fa5acc16390c2a0fa26a773302b9cb
Abdullah Albanna <albannaa78@gmail.com>
2024-05-29 18:39:28 +0300

    Create __init__.py

---
+++ Scripts/__init__.py
@@ -0,0 +1,5 @@
+# For relative imports to work in Python 3.6
+#import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# bundle_dir = (sys._MEIPASS if hasattr(sys, '_MEIPASS') else os.path.abspath('.')) 
+
+## This was replaced later on, just leaving it here, because why not! :)
