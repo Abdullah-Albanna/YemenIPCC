dfbc2883c7a26a040b78673fdfd0688777c5bdaa
Abdullah-Albanna <albannaa78@gmail.com>
2024-06-16 19:11:15 +0000

    Added improvements to logging and new buttons
    Fixed the logging always scrolling to the end with a thread, which is not necassary, instead scroll to the end if there is a new log.
    A button to disable/enable validating.
    An about section.
    And also some logging

--- Scripts/main_window.py
+++ Scripts/main_window.py
@@ -2,24 +2,35 @@ from .changing_option import (changeWhichOne, changeBundle)
 from .check_for_update import (checkForUpdateButton)
 from .injection import (cleanRemove)
 from .repair import (repairiPhone)
-from .projectimports import (tk, Thread, sleep, List, os, system, DPIResize) 
+from .thread_starter import startThread
+from .variables_manager import VariableManager
+
+from .projectimports import (tk, List, os, system, DPIResize, sleep, webbrowser, messagebox) 
+from .logging_config import setupLogging
+import logging
 
 
-def scrollLogText() -> None:
-    """
-    Continuously scrolls the log text widget to the end.
-    """
-    while True:
-        sleep(0.3)
-        log_text.see(tk.END)
+setupLogging(debug=True, file_logging=True)
 
-def logTextThread() -> None:
-    """
-    Starts a thread to continuously scroll the log text.
-    """
-    Thread(target=scrollLogText, daemon=True).start()
+def saveVariableInfo(variable_name, variable_info):
+        # Load existing variables from the text file
+    try:
+        temp_variables = VariableManager().loadTempVariables()
+    except Exception as e:
+        logging.error(f"updating_status.py - Could not load the saved bundle variables, error: {e}")
+
+    # Update or add the selected bundle name to the saved variables
+    temp_variables[f'{variable_name}'] = variable_info
+    
+    # Save the updated variables to the text file
+    try:
+        VariableManager().saveVariables(temp_variables)
+    except Exception as e:
+        logging.error(f"updating_status.py - Could not save bundle option, error: {e}")
+
 
 def logText(app_font: tk.font) -> tk.Text:
+
     """
     Creates and configures the log text widget.
     
@@ -28,20 +39,25 @@ def logText(app_font: tk.font) -> tk.Text:
     """
     global log_text
     # Create log text widget
-    log_text = tk.Text(log_frame, font=(app_font, DPIResize(12)), wrap="word", bg="#0a1750", fg="white", width=35)
-    log_text.pack(side=tk.LEFT, fill="both")
+    try:
+        log_text = tk.Text(log_frame, font=(app_font, DPIResize(12)), wrap="word", bg="#0a1750", fg="white", width=35)
+        log_text.pack(side=tk.LEFT, fill="both")
+    except Exception as e:
+        logging.error(f"main_window.py - An error occurred while packing the log_text, error: {e}")
 
     # Create scrollbar for log text widget
-    log_scrollbar: tk.Scrollbar = tk.Scrollbar(log_frame, command=log_text.yview, width=1, troughcolor="#030b2c")
-    log_scrollbar.pack(side=tk.RIGHT, fill="y")
+    try:
+        log_scrollbar: tk.Scrollbar = tk.Scrollbar(log_frame, command=log_text.yview, width=1, troughcolor="#030b2c")
+        log_scrollbar.pack(side=tk.RIGHT, fill="y")
+    except Exception as e:
+        logging.error(f"main_window.py - An error occurred while packing the log_scrollbar, error: {e}")
 
     log_text.config(yscrollcommand=log_scrollbar.set)
-    
-    logTextThread()
 
     return log_text
 
 def radioButtons(which_one: List[str], bundles: List[str], x: tk.IntVar, y: tk.IntVar, app_font: tk.font) -> None:
+
     """
     Creates radio buttons for selecting which option and bundle to choose.
     
@@ -55,6 +71,7 @@ def radioButtons(which_one: List[str], bundles: List[str], x: tk.IntVar, y: tk.I
 
     # Create radio buttons for options
     for index in range(len(which_one)):  
+
         radio_which_one_buttom: tk.Radiobutton = tk.Radiobutton(whichoneframe,
                                 text=which_one[index],
                                 variable=y,
@@ -74,6 +91,7 @@ def radioButtons(which_one: List[str], bundles: List[str], x: tk.IntVar, y: tk.I
 
     # Create radio buttons for bundles
     for index in range(len(bundles)):  
+
         radio_button: tk.Radiobutton = tk.Radiobutton(selectorframe,
                                 text=bundles[index],
                                 variable=x,
@@ -93,6 +111,7 @@ def radioButtons(which_one: List[str], bundles: List[str], x: tk.IntVar, y: tk.I
     
 
 def ContainerTypeLabel() -> None:
+
     """
     Creates a label for indicating the type of .ipcc.
     """
@@ -100,6 +119,7 @@ def ContainerTypeLabel() -> None:
 
 
 def frames(window: tk.Tk) -> tk.Frame:
+
     """
     Creates and configures different frames within the window.
     
@@ -135,6 +155,7 @@ def frames(window: tk.Tk) -> tk.Frame:
 
 
 def windowCreation(window: tk.Tk) -> None:
+
     """
     Configures the main window.
     
@@ -144,10 +165,11 @@ def windowCreation(window: tk.Tk) -> None:
     # Gets the resolution of the device and set it to it and subtract from it a little so it won't go full screen
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
+    logging.debug(f"main_window.py - Screen resolution: {screen_width}:{screen_height}")
     window.geometry(f"{round(screen_width * 0.80)}x{round(screen_height * 0.80)}")
 
 
-    window.title("Yemen IPCC - by Abdullah Albanna")
+    window.title("Yemen IPCC - by Abdullah Al-Banna")
     window.config(background="#030b2c") # Dark Blue
     path = os.path.join("_internal", "Scripts", "Images", "YemenIPCC.png")
     if os.path.exists(path):
@@ -159,6 +181,15 @@ def windowCreation(window: tk.Tk) -> None:
     window.withdraw() 
     
 def menuBarCreation(window: tk.Tk, current_version: str, app_font: tk.font) -> None:
+
+    def on_validate_toggle():
+        if validate_var.get():
+            saveVariableInfo("validate", "True")
+        else:
+            if messagebox.askyesno("Disable Validation", "Turning off validating would make development slow, it is recommended to leave it on\n\n Are you sure you want to disable it?"):
+                saveVariableInfo("validate", "False")
+            else:
+                validate_var.set(True)
     """
     Creates and configures the menu bar.
     
@@ -169,23 +200,35 @@ def menuBarCreation(window: tk.Tk, current_version: str, app_font: tk.font) -> N
 
     menubar = tk.Menu(window)
     window.config(menu=menubar, bg="#0a1750") # Lighter than the Dark Blue
+    validate_var = tk.BooleanVar(value=True)  # Checked by default
     
 
     mainmenu = tk.Menu(menubar, tearoff=0, font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)))
     toolsmenu = tk.Menu(menubar, tearoff=0, font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)))
+    helpmenu = tk.Menu(menubar, tearoff=0, font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)))
+    
 
     # It looks bad in Macs :(
     if system != "Mac":
         menubar.add_cascade(label="Main", menu=mainmenu, font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)))
         mainmenu.add_command(label="exit", font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)), command=window.quit)
 
-    
-
     menubar.add_cascade(label="Tools", menu=toolsmenu, font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)))
-
-
     toolsmenu.add_command(label="clean", font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)), command=lambda: cleanRemove(window, log_text, app_font))
     toolsmenu.add_command(label="re-pair", font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)), command=lambda: repairiPhone(log_text))
+    # Add a checkbutton to the Tools menu
+    toolsmenu.add_checkbutton(
+        label="validate",
+        font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)),
+        variable=validate_var,
+        command=on_validate_toggle
+    )
 
     toolsmenu.add_separator()
     toolsmenu.add_command(label="check for updates", font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)), command=lambda: checkForUpdateButton(current_version))
+
+    menubar.add_cascade(label="Help", menu=helpmenu, font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)))
+    helpmenu.add_command(label="github", font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)), command=lambda: webbrowser.open_new_tab("https://github.com/Abdullah-Albanna/YemenIPCC"))
+    if system != "Mac":
+        helpmenu.add_separator()
+        helpmenu.add_command(label="about", font=(app_font, 14 if system == "Mac" or system == "Linux" else DPIResize(10)), command= lambda: messagebox.showinfo("About", f"Yemen IPCC \n\n A simple app to automate the process of injecting the network configuration files (.ipcc) to the iPhone devices in Yemen\n\n\n author: Abdullah Al-Banna \n version: {current_version}"))
